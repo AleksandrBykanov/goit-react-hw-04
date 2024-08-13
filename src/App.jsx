a242270061c3908fc0404b670e50572c1bfd92f4
Aleksandr Bykanov <aleksandrbykanov1986@gmail.com>
2024-08-13 11:42:50 +0300

    get search

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import Loader from "./Loader/Loader";
 import ErrorMessage from "./ErrorMessage/ErrorMessage";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import './App.css'
 
 function App() {
   const [images, setImages] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
-  
+  const [search, setSearch] = useState(null);
+
   useEffect(() => {
     async function fetchImages() {
       try {
@@ -27,9 +29,31 @@ function App() {
     fetchImages();
   }, []);
 
+  useEffect(() => {
+    if (search === null) return;
+    async function fetchImagesByData() {
+      try {
+        setLoading(true);
+        const { data } = await axios.get(
+          `https://api.unsplash.com/search/photos?client_id=1FrxQdxZqIzEvaS9vHGUY2G62nXyYi7w7y_S4vo6_gg&query=${search.search}`
+        );
+        setImages(data.results);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchImagesByData();
+  }, [search]);
+
+  const onSubmit = (value) => {
+    setSearch(value);
+  };
+
   return (
     <div>
-      <SearchBar />
+      <SearchBar onSubmit={onSubmit} />
       {error !== null && (
         <p style={{ color: "red" }}>{error} Please, try again later.</p>
       )}
--- src/SearchBar/SearchBar.jsx
+++ src/SearchBar/SearchBar.jsx
@@ -1,17 +1,39 @@
-import css from "../SearchBar/SearchBar.module.css"
-const SearchBar = () => {
+import css from "../SearchBar/SearchBar.module.css";
+import { Field, Form, Formik, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
+const FeedbackSchema = Yup.object().shape({
+  search: Yup.string()
+    .min(2, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+});
+
+const SearchBar = ({ onSubmit }) => {
+  const handleSubmit = (values) => {
+    onSubmit(values);
+  };
+
   return (
-    <header>
-      <form className={css.form}>
-        <input
+    <Formik
+      initialValues={{
+        search: "",
+      }}
+      onSubmit={handleSubmit}
+      validationSchema={FeedbackSchema}
+    >
+      <Form className={css.form}>
+        <Field
+          name="search"
           type="text"
           autoComplete="off"
           autoFocus
           placeholder="Search images and photos"
         />
+        <ErrorMessage className={css.error} name="search" component="span" />
         <button type="submit">Search</button>
-      </form>
-    </header>
+      </Form>
+    </Formik>
   );
 };
 
